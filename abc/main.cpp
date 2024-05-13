bd081da42343f7348039a1ea2a85ab4bc24ad87f
solidskkim <sk.kim@solid.co.kr>
2024-05-13 10:43:04 +0900

    first commit

---
+++ abc/main.cpp
@@ -0,0 +1,29 @@
+/*
+ * main.cpp
+ *
+ *  Created on: 2024. 5. 13.
+ *      Author: sk.kim
+ */
+
+/* Includes ------------------------------------------------------------------*/
+#include <stdio.h>
+#include <iostream>
+using namespace std;
+
+
+
+/* Functions ------------------------------------------------------------------*/
+int main(void)
+{
+    cout << "Hello, world!" << endl;
+    cout << "abc" << endl;
+
+
+    return 0;
+}
+
+
+
+
+
+
